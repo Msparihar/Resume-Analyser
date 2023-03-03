20382958c498de47ec04e6f9f82bbca92d5c39ea
Msparihar <manishsparihar2020@gmail.com>
2023-03-03 22:09:27 +0530

    Implemented file upload system

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, redirect,url_for, request, render_template, jsonify
+import os
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired
+
 
 app=Flask(__name__)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload File")
+
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    name=""
-    return render_template("home.html", transcription=name)
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+        return "File has been uploaded."
+    return render_template("base.html", form=form)
+
+@app.route("/hello", methods = ['GET', 'POST'])
+def hello():
+    return "Hello There!"
 
 if __name__=="__main__":
     app.run(debug=True)
+    
+    
